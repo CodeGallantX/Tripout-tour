7e44cae0a60b3ba439920bfa739f9515bfcaa211
John A. Samuel <johnayobami77@proton.me>
2024-11-19 14:16:17 +0100

    Updated slider

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,8 +1,52 @@
 import members from '../data/team.json';
 import { Link } from 'react-router-dom';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa6';
 
 const Team = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: true,
+    speed: 500,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          }
+        ]
+      };
     return (
         <div>
             <section id="aboutIntro" className="px-10 xl:px-36 py-20">
@@ -18,10 +62,11 @@ const Team = () => {
                             </h2>
                         </div>
                         <div>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+                            <div className="grid grid-cols-1 gap-8">
+                                        <Slider {...settings}>
                                 {members.map((member, index) => (
                                     <div key={index} className="relative group">
-                                        <div className="relative flex flex-col items-center cursor-pointer justify-center py-6 px-8 rounded-xl border border-gray-400 hover:shadow-lg transition-all duration-300 ease-in-out">
+                                        <div className="w-full max-w-xl mx-4 relative flex flex-col items-center cursor-pointer justify-center py-6 px-8 rounded-xl border border-gray-400 hover:shadow-lg transition-all duration-300 ease-in-out">
                                             <div className="w-52 h-52">
                                             <img src={member.image} alt={member.name} className="w-full h-full object-cover object-center rounded-full" />
                                             </div>
@@ -50,6 +95,7 @@ const Team = () => {
                                         </div>
                                     </div>
                                 ))}
+                                    </Slider>
                             </div>
                         </div>
                     </div>
